7f52714313c0614c0188f9228eb52aac7c8f5909
ck198 <charmevillanueva1@gmail.com>
2025-03-21 18:42:08 +0800

    Update and rename Coding_Problem_1 to Coding_Problem_1.js

---
+++ CS1A_MTExam_VILLANUEVA/Coding_Problem_1.js
@@ -0,0 +1,5 @@
+console.log("User Profile:");
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
