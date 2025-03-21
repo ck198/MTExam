719dc69465e8a8aef728c2f211811bdd9b59bd1b
ck198 <charmevillanueva1@gmail.com>
2025-03-21 18:52:28 +0800

    Create Coding_Problem_5.js

---
+++ CS1A_MTExam_VILLANUEVA/Coding_Problem_5.js
@@ -0,0 +1,9 @@
+let colors = [];
+
+for (let i = 0; i < 3; i++) {
+    let color = prompt(`Enter color ${i + 1}:`); 
+    colors.push(color); 
+    
+   
+    console.log("Updated color list:", colors);
+}
